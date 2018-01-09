15d36825630643c6fb49b92b292d2869e1cccb5a
adayc <adayc@hotmail.com>
2018-01-09 08:06:55 -0500

    Agregando modal

--- js/planning.js
+++ js/planning.js
@@ -17,15 +17,16 @@ $(document).ready(function() {
 
   // Obteniendo los datos según el hotel seleccionado
   $('#list-hotels').on('input', function() {
-
     var $nameHotel = $(this).val();
     // Limpiando los campos cada vez que elegimos una nueva opcion  
     $('#stars').html('');
     $('#wifi').html('');
     $('#tripadvisor').html('');
     $('#price-hotel').html('');
-    //Obteniendo costo del hotel
-    $hotelPrice=0;
+    $('#breakfast').html('');
+   $('#address').html('');
+    // Obteniendo costo del hotel
+    $hotelPrice = 0;
   
     firebase.database().ref('place/' + city + '/hotels/' + $nameHotel).on('value', function(snap) {
       var rating = snap.val()['rating'];
@@ -35,19 +36,23 @@ $(document).ready(function() {
         $('#stars').append($stars);
       }
 
-      var $wifi= snap.val()['Wi-fi'];
+      var $wifi = snap.val()['Wi-fi'];
       $('#wifi').append($wifi);
-      var $tripadvisor= snap.val()['tripadvisor'];
+      var $tripadvisor = snap.val()['tripadvisor'];
       $('#tripadvisor').append('<p><a target="_blank" href="' + $tripadvisor + '"> Míralo en Tripadvisor</a></p>');
-      var $hotelPrice= snap.val()['precio-dia'];
-      console.log($hotelPrice);
+      var $hotelPrice = snap.val()['precio-dia'];
       $('#price-hotel').append($hotelPrice);
+
+      var $breakfast = snap.val()['Desayuno-incluido'];
+      $('#breakfast').append($breakfast);
+
+      var $address = snap.val()['address'];
+      var $iframe = snap.val()['iframe'];
+      $('#address').append('<p><a target="" href="#">'+ $address +'</a></p>');
     });
 
-    
-
-
-
+//Llamando al  modal
+$('#modal-hotel').modal('show');
     /* if ($('#list-kind-of-food').val().length === 0) {
       $('#btn-search').prop('disabled', true);
     } else {
