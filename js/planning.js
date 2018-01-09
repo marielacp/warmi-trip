b02c0ec4b7c9cfd196cf0d826488467e4169b8ae
adayc <adayc@hotmail.com>
2018-01-09 03:31:32 -0500

    Obteniendo data según firebase

--- js/planning.js
+++ js/planning.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
   $('.datepicker').datepicker();
-
+  var city = 'Cuzco';
   
-});
+
+ 
+  firebase.database().ref('place/'+city+'/hotels').on('value', function(snap) {
+    console.log(snap.val());
+    var hotels = Object.keys(snap.val());
+   
+    console.log(hotels);
+  
+   
+  });
+
+}); 
