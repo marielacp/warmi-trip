a69499df730cfafab524561691b3ec15801d8157
adayc <adayc@hotmail.com>
2018-01-09 03:44:25 -0500

    Cargamos hoteles según ciudad elegida

--- js/planning.js
+++ js/planning.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
   $('.datepicker').datepicker();
   var city = 'Cuzco';
-  
 
  
-  firebase.database().ref('place/'+city+'/hotels').on('value', function(snap) {
+  firebase.database().ref('place/' + city + '/hotels').on('value', function(snap) {
     console.log(snap.val());
     var hotels = Object.keys(snap.val());
    
-    console.log(hotels);
-  
-   
+    for (var i = 0; i < hotels.length; i++) {
+      var $option = $('<option value="' + hotels[i] + '"></option>');  
+      $('#hotels').append($option);
+    }
   });
-
 }); 
