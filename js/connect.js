3881b71e926ed3839e73a0ebbf6f5c3778b46b97
adayc <adayc@hotmail.com>
2018-01-10 00:15:09 -0500

    Agregando connect.js

--- js/connect.js
+++ js/connect.js
@@ -1,10 +1,10 @@
-﻿  // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyBrTZrmws7ldzVUz01pEXfux1uPe5vID3Q",
-      authDomain: "warmi-trip-17f9d.firebaseapp.com",
-      databaseURL: "https://warmi-trip-17f9d.firebaseio.com",
-      projectId: "warmi-trip-17f9d",
-      storageBucket: "warmi-trip-17f9d.appspot.com",
-      messagingSenderId: "109955586871"
-    };
-    firebase.initializeApp(config);
+﻿// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyBrTZrmws7ldzVUz01pEXfux1uPe5vID3Q',
+  authDomain: 'warmi-trip-17f9d.firebaseapp.com',
+  databaseURL: 'https://warmi-trip-17f9d.firebaseio.com',
+  projectId: 'warmi-trip-17f9d',
+  storageBucket: 'warmi-trip-17f9d.appspot.com',
+  messagingSenderId: '109955586871'
+};
+firebase.initializeApp(config);
