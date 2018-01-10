5a2203a67d8c0969e69e4b78b5d9938cf941daa4
adayc <adayc@hotmail.com>
2018-01-10 17:11:36 -0500

    Identando perfil.js

--- js/perfil.js
+++ js/perfil.js
@@ -1,40 +1,35 @@
 $(document).ready(function() {
+  // Obteniendo el UID del localStorage
   var UID = window.localStorage.getItem('storageUID');
-  console.log(UID);
+  // Obteniendo la fecha actual
   var d = new Date();
   var $date = d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate();
-  // Cargando la foto del usuario
 
-  // Cargando los datos del usuario 
- 
+  // Leyendo los datos del usuario
   firebase.database().ref('bd').on('value', function(data) {
-
     var user = data.val();
-   
-    console.log(user);
-  })
+  });
 
-  //
- // "select-ciudad"
- 
 
- $('.btn-plan').click(function() {
+  // Guardando en el localstorage la ciudad que se elije
+  $('.btn-plan').click(function() {
+    window.localStorage.setItem('city', $('#select-ciudad').val());
 
-  window.localStorage.setItem('city',$('#select-ciudad').val());
-  console.log('botones')
-  alert (window.localStorage.getItem('city'))
- });
+    alert(window.localStorage.getItem('city'));
+  });
 
   // Obteniendo imagenes del input file
   $('#btn-save-file').click(function() {
     var $fileName = document.getElementById('txt-file').files[0]['name'];
-  
+
     var $description = $('#txt-description').val();
+    // Guardando los posts en el muro
     $('#container-post').append('<img class="img-responsive img-rounded post-foto center-block" src="../assets/img/' + $fileName + '">');
-    
+
     $('#container-post').append('<p class="text-center">' + $description + '</p>');
+    // Guardando los posts en la base de datos - fotos
     firebase.database().ref('posts/' + UID).push(
-      
+
       {
         photo: $fileName,
         description: $description,
@@ -42,15 +37,15 @@ $(document).ready(function() {
       });
     $(this).hide();
   });
-
+  // Guardando los posts en la base de datos .posts
   $('#btn-save-post').click(function() {
     var $post = $('#txt-post').val();
-       
+
     $('#container-post').append('<p class="text-center">' + $post + '</p>');
     firebase.database().ref('posts/' + UID).push({
       publish: $post,
       date: $date
-      
+
     });
 
     $(this).hide();
