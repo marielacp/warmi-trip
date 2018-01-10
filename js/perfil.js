6d6e4287fd8fd1cd2b31c8c051118cc880e2b96f
adayc <adayc@hotmail.com>
2018-01-10 11:20:46 -0500

    Agregando modal de posts

--- js/perfil.js
+++ js/perfil.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-
-
+  // Obteniendo imagenes del input file
+  $('#btn-save-file').click(function() {
+    var $fileName = document.getElementById('txt-file').files[0]['name'];
+    alert($fileName);
+    var $description = $('#txt-description').val();
+    $('#container-post').append('<img class="img-responsive img-rounded post-foto center-block" src="../assets/img/'+$fileName +'">')
+    
+    $('#container-post').append('<p class="text-center">'+$description+'</p>')
+    
+  });
 }); 
