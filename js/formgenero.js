8538d244d18f31d4b18b9b6399fbedae56e4383f
adayc <adayc@hotmail.com>
2018-01-10 17:03:28 -0500

    Identando formgenero

--- js/formgenero.js
+++ js/formgenero.js
@@ -1,13 +1,13 @@
 
 /* creando funcion global con JQuery*/
-$(document).ready(function() { 
+$(document).ready(function () {
   var $genero = $('#select-genero');
-  var $address = $('#address'); 
+  var $address = $('#address');
   var $checkbox = $('input[type="checkbox"]');
   var $submit = $('#submit');
 
   /* Validando genero */
-  $genero.change(function() {
+  $genero.change(function () {
     if ($(this).val() === 'fem') {
       console.log('ok');
     } else {
@@ -15,19 +15,19 @@ $(document).ready(function() {
     }
   });
 
-  /* Validando datos llenos en direcciòn*/
-  $address.on('input', function(event) {
+  /* Validando datos llenos en dirección*/
+  $address.on('input', function (event) {
     /* Comprobando que no ingrese vacios o que no ingrese datos number*/
-    if (($(this).val() == '')) {
+    if (($(this).val() === '')) {
       $(this).val('');
-      alert('Ingrese su direccion');     
+      alert('Ingrese su direccion');
     }
   });
 
   /* Validando boton submit genero*/
-  $submit.click(function(event) {
-    /* console.log(event.target.checked) */
-    if ($checkbox.prop('checked') && ($genero.val() === 'fem') && ($address.val()!== '')) {
+  $submit.click(function (event) {
+
+    if ($checkbox.prop('checked') && ($genero.val() === 'fem') && ($address.val() !== '')) {
       event.preventDefault();
       window.location.href = '../views/login.html';
     } else {
