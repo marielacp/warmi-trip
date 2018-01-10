0608cc74e15425eddd1b4be538430756f088ec51
adayc <adayc@hotmail.com>
2018-01-10 04:57:01 -0500

    Modificando login.html

--- js/login.js
+++ js/login.js
@@ -5,11 +5,9 @@ $('#login').click(function() {
   firebase.auth().signInWithPopup(provider).then(function(result) {
     console.log(result);
     guardaDatos(result.user);
-    $('#login').hide();
-    $('#root').append("<img src='" + result.user.photoURL + "'/ >");
+    
   }); 
 });
-
 /* funcion guardar datos */
 function guardaDatos(user) {
   var usuario = {
@@ -19,22 +17,7 @@ function guardaDatos(user) {
     photo: user.photoURL,
   };
   firebase.database().ref('bd/ ' + user.uid)
-    .set(usuario)
+    .set(usuario);
 }
 
-/* guardando bd */
-$('#guardar').click(function() {
-  firebase.database().ref('prueba')
-    .set({
-      nombre: 'mariela',
-      edad: '15',
-      sexo: 'femenino',
-    });
-});
 
-/* agregando todas las fotos de inicio de secion de usuarios*/
-firebase.database().ref('bd')
-  .on('child_added', function(s) {
-    var user = s.val();
-    $('#root').append("<img width= '100px' src= '" + user.photo + "'/>");
-  });
