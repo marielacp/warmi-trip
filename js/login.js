7f9a18a4198ec09e885ef34b1623641b95a7ff41
adayc <adayc@hotmail.com>
2018-01-10 17:06:44 -0500

    Identando login

--- js/login.js
+++ js/login.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
-/* logueando js con google. */
+  /* logueando js con cuenta de google. */
   var provider = new firebase.auth.GoogleAuthProvider();
 
   $('#login').click(function() {
+    // Aparece la ventana pop-up que permite validar
     firebase.auth().signInWithPopup(provider).then(function(result) {
-      
-     
       guardaDatos(result.user);
-      window.localStorage.setItem('storageUID',result.user.uid );
+      window.localStorage.setItem('storageUID', result.user.uid);
       console.log(result.user);
-
-
+      // Guardando el UID en el localstorage
       var UID = window.localStorage.getItem('storageUID');
       console.log(UID);
-    // Creando el código de usuario en la rama de posts 
-      firebase.database().ref('posts').set({UID});
-      
+      // Creando el código de usuario en la rama de posts 
+      firebase.database().ref('posts').set({ UID });
+      // Redireccionando al perfil
       window.location.href = 'perfil.html';
-    }); 
+    });
   });
   /* funcion guardar datos */
   function guardaDatos(user) {
@@ -27,15 +25,10 @@ $(document).ready(function() {
       email: user.email,
       photo: user.photoURL,
     };
-    
-    
-    firebase.database().ref('bd/'+ user.uid).set(usuario);
-      
-          
+
+    // Guardando los datos con el user.id
+    firebase.database().ref('bd/' + user.uid).set(usuario);
   }
- 
- 
-  
 });
 
 
