592c16a8ee577a758f55d099f8fa21d14fb41af5
adayc <adayc@hotmail.com>
2018-01-10 17:14:45 -0500

    Identando planning.js

--- js/planning.js
+++ js/planning.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
   // result user
-
-
   // Obtenemos la ciudad elegida
   var $city = window.localStorage.getItem('city');
   var $UID = window.localStorage.getItem('storageUID');
@@ -13,14 +11,10 @@ $(document).ready(function () {
     $('#btn-lunch').attr('data-content', snap.val()['price']['lunch']);
     $('#btn-dinner').attr('data-content', snap.val()['price']['dinner']);
   });
-
-
   // Cargamos los datepickers en todos aquellos elementos que tengan la clase datepicker
   $('.datepicker').datepicker();
   // Habilitamos los popovers
   $('[data-toggle="popover"]').popover();
-
-
   // Obtenemos la data según la ciudad elegida
   firebase.database().ref('place/' + $city + '/hotels').on('value', function (snap) {
     // console.log(snap.val());
@@ -31,9 +25,7 @@ $(document).ready(function () {
       $('#hotels').append($option);
     }
   });
-
   // Obtenemos transporte según la ciudad elegida
-
   firebase.database().ref('transport').on('value', function (snap) {
     var $arrayTransport = Object.keys(snap.val());
     // Cargamos los hoteles según la ciudad elegida
@@ -42,13 +34,10 @@ $(document).ready(function () {
       $('#list-transport').append($option);
     }
   });
-
-
   // Obteniendo los datos según el hotel seleccionado
   $('#hotels').on('change', function () {
     var $nameHotel = $(this).val();
     // Limpiando los campos cada vez que elegimos una nueva opcion 
-
     $('#title-modal').html('');
     $('#modal-map').html('');
     $('#stars').html('');
@@ -59,10 +48,8 @@ $(document).ready(function () {
     $('#address').html('');
     // Obteniendo costo del hotel
     $hotelPrice = 0;
-
     firebase.database().ref('place/' + $city + '/hotels/' + $nameHotel).on('value', function (snap) {
       $('#title-modal').html(snap.val()['name']);
-
       $('#modal-map').append(snap.val()['iframe']);
       var $rating = snap.val()['rating'];
       // Insertando iconos de stars según la bd
@@ -77,21 +64,17 @@ $(document).ready(function () {
       $('#tripadvisor').append('<p><a target="_blank" href="' + $tripadvisor + '"> Míralo en Tripadvisor</a></p>');
       var $hotelPrice = snap.val()['precio-dia'];
       $('#price-hotel').append($hotelPrice);
-
       var $breakfast = snap.val()['Desayuno-incluido'];
       $('#breakfast').append($breakfast);
-
       var $address = snap.val()['address'];
       var $iframe = snap.val()['iframe'];
       $('#address').append('<p><a target="" href="#">' + $address + '</a></p>');
     });
-
     // Llamando al modal con la informacion  del hotel
     $('#modal-hotel').modal('show');
   });
   $('#list-transport').on('change', function () {
     var $companyName = $(this).val();
-
     // Obteniendo el costo del transporte
     firebase.database().ref('transport/' + $companyName + '/destination-place/' + $city).on('value', function (snap) {
       var $transportPrice = snap.val();
@@ -102,9 +85,7 @@ $(document).ready(function () {
       }
     });
   });
-
   // Guardando los datos del planning en la data de firebase
-
   $('#btn-save').click(function () {
     firebase.database().ref('trips/' + $UID).push(
       $UID = {
