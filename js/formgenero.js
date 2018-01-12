748141169e7f1be6002014a8189e20d4f8836d7b
adayc <adayc@hotmail.com>
2018-01-12 02:48:01 -0500

    Identando css genero

--- js/formgenero.js
+++ js/formgenero.js
@@ -1,31 +1,27 @@
-
 /* creando funcion global con JQuery*/
-$(document).ready(function() {
+$(document).ready(function () {
   var $genero = $('#select-genero');
   var $address = $('#address');
   var $checkbox = $('input[type="checkbox"]');
   var $submit = $('#submit');
-
   /* Validando genero */
-  $genero.change(function() {
+  $genero.change(function () {
     if ($(this).val() === 'fem') {
       console.log('ok');
     } else {
       console.log('no eres un fem');
     }
   });
-
   /* Validando datos llenos en dirección*/
-  $address.on('input', function(event) {
+  $address.on('input', function (event) {
     /* Comprobando que no ingrese vacios o que no ingrese datos number*/
     if (($(this).val() === '')) {
       $(this).val('');
       alert('Ingrese su direccion');
     }
   });
-
   /* Validando boton submit genero*/
-  $submit.click(function(event) {
+  $submit.click(function (event) {
     if ($checkbox.prop('checked') && ($genero.val() === 'fem') && ($address.val() !== '')) {
       event.preventDefault();
       window.location.href = '../views/login.html';
