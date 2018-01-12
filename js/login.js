f7a63105287be726adc7040c06660e10f707c5fe
adayc <adayc@hotmail.com>
2018-01-12 06:20:23 -0500

    Set y push en firebase

--- js/login.js
+++ js/login.js
@@ -3,31 +3,35 @@ $(document).ready(function() {
   var provider = new firebase.auth.GoogleAuthProvider();
 
   $('#login').click(function() {
+   
+    
+
     // Aparece la ventana pop-up que permite validar
     firebase.auth().signInWithPopup(provider).then(function(result) {
-      guardaDatos(result.user);
+      
+      saveData(result.user);
       window.localStorage.setItem('storageUID', result.user.uid);
-      console.log(result.user);
+   
       // Guardando el UID en el localstorage
       var UID = window.localStorage.getItem('storageUID');
       console.log(UID);
       // Creando el código de usuario en la rama de posts 
       firebase.database().ref('posts').set({ UID });
       // Redireccionando al perfil
-      window.location.href = 'perfil.html';
+     //window.location.href = 'perfil.html';
     });
   });
-  /* funcion guardar datos */
-  function guardaDatos(user) {
-    var usuario = {
+  function saveData(user) {
+    console.log(user);
+    var userToSave = {
       uid: user.uid,
       name: user.displayName,
       email: user.email,
-      photo: user.photoURL,
+      photo: user.photoURL
     };
-
-    // Guardando los datos con el user.id
-    firebase.database().ref('bd/' + user.uid).set(usuario);
+    // probando es el nombre de tu rama
+    
+    firebase.database().ref('bd/' + user.uid).set(userToSave).push(userToSave); // push añade un registro 
   }
 });
 
