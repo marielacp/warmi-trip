3fe7cfe8ed13f71839a8e7d535cfcc67fd32ac5d
adayc <adayc@hotmail.com>
2018-01-12 06:43:00 -0500

    Añadiendo boton continuar el login

--- js/login.js
+++ js/login.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
+  $('#next').hide();
   /* logueando js con cuenta de google. */
   var provider = new firebase.auth.GoogleAuthProvider();
-
   $('#login').click(function() {
-   
-    
-
     // Aparece la ventana pop-up que permite validar
     firebase.auth().signInWithPopup(provider).then(function(result) {
-      
       saveData(result.user);
       window.localStorage.setItem('storageUID', result.user.uid);
-   
       // Guardando el UID en el localstorage
       var UID = window.localStorage.getItem('storageUID');
       console.log(UID);
       // Creando el código de usuario en la rama de posts 
       firebase.database().ref('posts').set({ UID });
       // Redireccionando al perfil
-     //window.location.href = 'perfil.html';
+      $('#login').hide();
+      $('#next').show();
     });
+    
   });
+
+
+
   function saveData(user) {
     console.log(user);
     var userToSave = {
@@ -30,8 +30,8 @@ $(document).ready(function() {
       photo: user.photoURL
     };
     // probando es el nombre de tu rama
-    
-    firebase.database().ref('bd/' + user.uid).set(userToSave).push(userToSave); // push añade un registro 
+
+    firebase.database().ref('bd/' + user.uid).set(userToSave); // push añade un registro 
   }
 });
 
