a3b6febfe019fa0a5b4de241005dd4c5960decbf
adayc <adayc@hotmail.com>
2018-01-15 10:55:35 -0500

    Dando estilos a los posts

--- js/profile.js
+++ js/profile.js
@@ -15,19 +15,55 @@ $(document).ready(function() {
 
   // Leyendo los datos del viaje del usuario
   firebase.database().ref('trips/' + UID).on('value', function(snap) {
-    arrTrip = Object.keys(snap.val());
-    lastTrip = snap.val()[arrTrip[arrTrip.length - 1]];
+    var arrTrip = Object.keys(snap.val());
+    var lastTrip = snap.val()[arrTrip[arrTrip.length - 1]];
     
     // console.log(lastTrip['transporte']);
     $('#trip-city').html('<strong>Lugar:</strong> ' + lastTrip['place']);
     $('#trip-date').html('<strong>Del:</strong> ' + lastTrip['fechaDePartida'] + ' <strong>al </strong>' + lastTrip['fechaDeLlegada']);
-
-    /* $('#img-perfil').attr('src', snap.val()['photo']);
-    $('#img-ico-perfil').attr('src', snap.val()['photo']);
-    $('#txt-edit-name').attr('value', snap.val()['name']);
-    $('#txt-edit-email').attr('value', snap.val()['email']);*/
   });
 
+
+  // Cargando imagenes del localstorage según el id de usuario
+
+
+  firebase.database().ref('photos/' + UID).on('value', function(snapshot) {
+    var phototArray = Object.keys(snapshot.val());
+    
+
+    for (i = 0;i <phototArray.length ;i++) {
+     
+
+      var $newDivPhoto = $('<div class=\'post-perfil\'></div>');
+      $newDivPhoto.append('<p class="date-post">' + snapshot.val()[phototArray[i]]['date'] + '</p>');
+      $newDivPhoto.append('<img class="img-responsive img-rounded post-foto center-block" src="' + snapshot.val()[phototArray[i]]['photo'] + '">');
+  
+      $newDivPhoto.append('<p class="text-center">' + snapshot.val()[phototArray[i]]['description'] + '</p>');
+      $('#container-photo div:first-child').before($newDivPhoto);
+
+      console.log(snapshot.val()[phototArray[i]]['photo']);
+      console.log(snapshot.val()[phototArray[i]]['description']);
+      console.log(snapshot.val()[phototArray[i]]['date']);
+    }
+  });
+// Cargando posts del usuario
+  firebase.database().ref('posts/' + UID).on('value', function(snapshot) {
+    var postArray = Object.keys(snapshot.val());
+    
+
+    for (i = 0;i <postArray.length ;i++) {
+     
+
+      var $newDivPost = $('<div class=\'post-perfil\'></div>');
+      $newDivPost.append('<p class="date-post">' + snapshot.val()[postArray[i]]['date']+ '</p>');
+      $newDivPost.append('<p class="date-post">' + snapshot.val()[postArray[i]]['publish']+ '</p>');
+      // $('#container-post').children(0).before($newDivPost);
+      
+  
+      $('#container-post div:first-child').before($newDivPost);
+
+    }
+  });
 
   // Creando la variable city en el storage
   $('#btn-plan').click(function() {
@@ -49,7 +85,7 @@ $(document).ready(function() {
   $('#btn-save-file').click(function() {
     // Referenciando al storage nodo raiz
     var $storageRef = firebase.storage().ref();
-    console.log($storageRef);
+    // console.log($storageRef);
     
  
     // Asignamos nombre al archivo y le colocamos la funcion Math, en el caso que se posteen fotos con el mismo nombre
@@ -100,7 +136,7 @@ $(document).ready(function() {
       $('#myModal').modal('hide');
     });
   });
-// Guardar publicaciones 
+  // Guardar publicaciones 
   $('#btn-save-post').click(function() {
     var $post = $('#txt-post').val();
     var $newDivPost = $('<div class=\'post-perfil\'></div>');
